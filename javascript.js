1a6946c4438103848703e3709be46dfce2c83066
elcharbs <60406297+elcharbs@users.noreply.github.com>
2023-12-25 18:03:48 -0500

    Add initial file html structure and create add and subtract functions

---
+++ javascript.js
@@ -0,0 +1,9 @@
+console.log('Hello world!');
+
+let sum = function (...theArgs) {
+    return theArgs.reduce((sum, current) => sum + current, 0);
+};
+
+let subtract = function (...theArgs) {
+    return theArgs.reduce((sum, current) => sum - current);
+};
