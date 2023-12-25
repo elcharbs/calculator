d4cb381d4aba02c70ccdfeaf8111ba607d4378af
elcharbs <60406297+elcharbs@users.noreply.github.com>
2023-12-25 18:33:02 -0500

    Create operate function using Switch statements

--- javascript.js
+++ javascript.js
@@ -17,3 +17,22 @@ let divide = function (...theArgs) {
 };
 
 let var1 = null, var2 = null, operator = null;
+
+let validOperators = ['x', '*', '/', '+', '-'];
+
+let operate = function (variableOne, variableTwo, theOperator) {
+    let output = 0;
+    switch (validOperators.findIndex((x) => x === theOperator)) {
+        case 0:
+        case 1:
+            return multiply(variableOne, variableTwo);
+        case 2:
+            return divide(variableOne, variableTwo);
+        case 3:
+            return sum(variableOne, variableTwo);
+        case 4:
+            return subtract(variableOne, variableTwo);
+        default:
+            return 'ERROR';
+    }
+};
