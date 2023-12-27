5c840f57a9c87069e94907169c434ea4755d6cb6
elcharbs <60406297+elcharbs@users.noreply.github.com>
2023-12-27 11:54:43 -0500

    Comment out code to attempt rebuild of calculator functionality

--- javascript.js
+++ javascript.js
@@ -42,23 +42,43 @@ const buttons = document.querySelectorAll(".calculatorButton");
 const displayBox = document.querySelector(".displayBox");
 let displayBoxValue = null;
 let output = null;
+let lastButtonOperator = false;
 
 buttons.forEach(element => {
     element.addEventListener('click', () => {
-        if (element.innerHTML === 'CLEAR' || displayBox.innerHTML === 'ERROR') {
+        if (element.innerHTML === 'CLEAR' || displayBox.innerHTML === 'ERROR' || displayBox.innerHTML === 'NaN') {
             displayBox.innerHTML = 0;
-            var1, var2, operator = null;
+            var1 = null;
+            var2 = null;
+            operator = null;
+            console.log(var1 + ' ' + var2 + ' ' + operator);
         } else if (validOperators.includes(element.innerHTML)) {
-            var1 = Number(displayBox.innerHTML);
-            operator = element.innerHTML;
-            displayBox.innerHTML = 0;
-            console.log('After operator button var1 is ' + var1 + ' var2 is ' + var2 + ' the operator is ' + operator + ' and the output is ' + output);
+            if (var1 !== null) {
+                var2 = Number(displayBox.innerHTML);
+                operator = element.innerHTML;
+                output = operate(var1, var2, operator); 
+                displayBox.textContent = output;
+                console.log(var1 + ' is var1');
+                console.log(var2 + ' is var2');
+                var2 = null;
+                lastButtonOperator = true;
+            } else {
+                var1 = Number(displayBox.innerHTML);
+                operator = element.innerHTML;
+                displayBox.innerHTML = 0;
+                console.log('After operator button var1 is ' + var1 + ' var2 is ' + var2 + ' the operator is ' + operator + ' and the output is ' + output);
+            }
         } else if (integers.includes(element.innerHTML)) {
-            displayBoxValue = (displayBox.textContent === '0') ? 
+            if (lastButtonOperator === true) {
+                displayBox.innerHTML = element.innerHTML;
+                lastButtonOperator = false;
+            } else {
+                displayBoxValue = (displayBox.textContent === '0') ? 
                 element.innerHTML : 
                 displayBox.textContent.concat(element.innerHTML);
             displayBox.innerHTML = displayBoxValue;
-            console.log('After integer button var1 is ' + var1 + ' var2 is ' + var2 + ' the operator is ' + operator + ' and the output is ' + output);
+            console.log('After integer button var1 is ' + var1 + ' var2 is ' + var2 + ' the operator is ' + operator + ' and the output is ' + output);    
+            }
         } else {
             var2 = Number(displayBox.innerHTML);
             output = operate(var1, var2, operator); 
